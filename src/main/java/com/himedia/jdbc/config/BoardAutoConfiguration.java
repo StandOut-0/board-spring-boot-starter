820f5a39e9a71f4b1767a30d7c50248a37f4c97b
StandOut <StandOut.first@gmail.com>
2023-04-27 10:38:29 +0900

    properties파일로 db정보를 빼고 정보를 받아 객체를 만들어 @ConfigurationProperties(prefix="board.jdbc"), 객체 JDBCConnectionManagerProperties를 get해 커넥션.

--- src/main/java/com/himedia/jdbc/config/BoardAutoConfiguration.java
+++ src/main/java/com/himedia/jdbc/config/BoardAutoConfiguration.java
@@ -1,13 +1,22 @@
 package com.himedia.jdbc.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import com.himedia.jdbc.util.JDBCConnectionManager;
 
-@Configuration //JDBCConnectionManager를 bean으로 등록하는 환경설정 class
+//@Configuration  JDBCConnectionManager를 bean으로 등록하는 환경설정 class
+//@EnableConfigurationProperties 활성화할 프로퍼티 클래스를 지정할때 사용
+@Configuration 
+@EnableConfigurationProperties(JDBCConnectionManagerProperties.class)  
 public class BoardAutoConfiguration {
+	
+	//활성화된 프로퍼티객체(JDBCConnectionManagerProperties)는 @autowired
+	@Autowired
+	private JDBCConnectionManagerProperties properties;
 
 	//@ConditionalOnMissingBean 해당 JDBCConnectionManager bean이 메모리에 없는 경우에만 빈 등록을 처리한다.
 	
@@ -15,10 +24,16 @@ public class BoardAutoConfiguration {
 	@ConditionalOnMissingBean
 	public JDBCConnectionManager getJDBCConnectionManager() {
 		JDBCConnectionManager manager = new JDBCConnectionManager();
-		manager.setDriverClass("oracle.jdbc.driver.OracleDriver");
-		manager.setUrl("jdbc:oracle:thin:@localhost:1521:orcl");
-		manager.setUsername("hr");
-		manager.setPassword("12341234");
+//		manager.setDriverClass("oracle.jdbc.driver.OracleDriver");
+//		manager.setUrl("jdbc:oracle:thin:@localhost:1521:orcl");
+//		manager.setUsername("hr");
+//		manager.setPassword("12341234");
+		
+		//데이터베이스 객체정보를 get한다.
+		manager.setDriverClass(properties.getDriverClass());
+		manager.setUrl(properties.getUrl());
+		manager.setUsername(properties.getUsername());
+		manager.setPassword(properties.getPassword());
 		return manager;
 	}
 
